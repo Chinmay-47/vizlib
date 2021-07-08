aaee114b68f3bce54dc48f989853f5078503bc23
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-07-08 11:21:56 +0530

    added a couple more tests and license

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,3 +1,22 @@
+"""
+
+ Copyright 2021 Chinmaya Srivatsa
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+"""
+
+
 import unittest
 from vizlib import vizutils
 import numpy as np
@@ -9,7 +28,17 @@ class TestDataPointsGenerator(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.dpgen = vizutils.DataPointsGenerator()
 
+    def test_assign_random_state(self):
+        with self.assertRaises(AttributeError):
+            self.dpgen.__setattr__('__random_state', 27)
+
+    def test_delete_random_state(self):
+        with self.assertRaises(AttributeError):
+            self.dpgen.__delattr__('__random_state')
+
     def test_set_random_state(self):
+        with self.assertRaises(ValueError):
+            self.dpgen.set_random_state(-3)
         self.dpgen.set_random_state(27)
         self.assertEqual(self.dpgen.random_state, 27)
         self.dpgen.set_random_state(0)
